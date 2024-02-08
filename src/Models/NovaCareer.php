f389b7195dfc1ca7c4789e58b08e186f36f244a9
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-02-08 11:44:56 +0000

    Utilise new components functionality from page builder.

--- src/Models/NovaCareer.php
+++ src/Models/NovaCareer.php
@@ -3,28 +3,16 @@
 namespace Creode\LaravelNovaCareers\Models;
 
 use Creode\LaravelCareers\Models\Career;
-use Creode\NovaPageBuilder\Services\BlockRenderer;
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use Creode\NovaPageBuilder\Traits\HasComponents;
 use Whitecube\NovaFlexibleContent\Value\FlexibleCast;
 
 class NovaCareer extends Career
 {
+    use HasComponents;
+
     protected $casts = [
         'description' => FlexibleCast::class,
     ];
 
-    /**
-     * Exposes a set of components from the Page Builder.
-     *
-     * @return Attribute
-     */
-    protected function components(): Attribute
-    {
-        $blockRendererService = app()->make(BlockRenderer::class);
-        return Attribute::make(
-            get: function () use ($blockRendererService) {
-                return $blockRendererService->render($this->description);
-            }
-        );
-    }
+    protected $componentField = 'description';
 }
