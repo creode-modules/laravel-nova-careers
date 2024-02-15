5a35bb3e5511b5d44b950de56d45e1f906eb6c05
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-02-15 14:01:23 +0000

    Adds a fix by adding a missing trait to NovaCareer class.

--- src/Models/NovaCareer.php
+++ src/Models/NovaCareer.php
@@ -4,15 +4,26 @@ namespace Creode\LaravelNovaCareers\Models;
 
 use Creode\LaravelCareers\Models\Career;
 use Creode\NovaPageBuilder\Traits\HasComponents;
+use Whitecube\NovaFlexibleContent\Concerns\HasFlexible;
 use Whitecube\NovaFlexibleContent\Value\FlexibleCast;
 
 class NovaCareer extends Career
 {
-    use HasComponents;
+    use HasComponents, HasFlexible;
 
+    /**
+     * Defines casts for model attributes.
+     *
+     * @var array
+     */
     protected $casts = [
         'description' => FlexibleCast::class,
     ];
 
+    /**
+     * Define the field used for components.
+     *
+     * @var string
+     */
     protected $componentField = 'description';
 }
